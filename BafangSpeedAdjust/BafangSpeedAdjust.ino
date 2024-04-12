6a9378275963fcd9347a91565e35a66fec067572
Finch666 <45091373+Finch666@users.noreply.github.com>
2024-04-12 11:24:41 +0200

    SetOriginalSpeed.ino

--- BafangSpeedAdjust/BafangSpeedAdjust.ino
+++ BafangSpeedAdjust/BafangSpeedAdjust.ino
@@ -5,12 +5,11 @@ struct can_frame canMsg;
 MCP2515 mcp2515(10);
 
 struct can_frame canMsg35;
-
-bool speedSet = false;
-
+const int buttonPin = 4;
+int buttonState = 0;   
 void setup() {
   Serial.begin(115200);
-  
+  pinMode(buttonPin, INPUT_PULLUP);
   mcp2515.reset();
   mcp2515.setBitrate(CAN_250KBPS, MCP_8MHZ);
   mcp2515.setNormalMode();
@@ -24,8 +23,8 @@ void setup() {
   
   canMsg35.can_id  = 0x85103203 | CAN_EFF_FLAG;
   canMsg35.can_dlc = 6;
-  canMsg35.data[0] = 0xAC;
-  canMsg35.data[1] = 0x0D;
+  canMsg35.data[0] = 0xC4;
+  canMsg35.data[1] = 0x09;
   canMsg35.data[2] = 0xC0;
   canMsg35.data[3] = 0x2B;
   canMsg35.data[4] = 0xCE;
@@ -33,25 +32,14 @@ void setup() {
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
-      Serial.print(canMsg.can_id, HEX); //Print ID
-      Serial.print(" "); 
-      Serial.print(canMsg.can_dlc, HEX); //Print DLC
-      Serial.print(" ");
-      
-      for (int i = 0; i<canMsg.can_dlc; i++)  { //Print the data
-        Serial.print(canMsg.data[i],HEX);
-        Serial.print(" ");
-      }
-  
-      Serial.println();
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == LOW) {
+    putback();
+  }
   }
 
   //Write speed setting after 10 seconds waiting
-  if(!speedSet && currentMillis > 10000){
-    speedSet = true;
+  void putback(){
     Serial.println("------ WRITING SPEED ------");
     Serial.println("------ WRITING SPEED ------");
     Serial.println("------ WRITING SPEED ------");
@@ -66,4 +54,3 @@ void loop() {
     Serial.println("----------- DONE ----------");
     Serial.println("----------- DONE ----------");
   }
-}
